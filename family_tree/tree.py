76c369ab7ad3230f14ff121cf810e79f6b8bda6f
ash <amurphy9956@live.com>
2018-08-04 00:00:35 +0700

    Added some error handling

--- family_tree/tree.py
+++ family_tree/tree.py
@@ -1,5 +1,5 @@
 WELCOME = "Welcome to the family, {}"
-CHILD_EXISTS = "Sorry that child already exists!"
+PERSON_EXISTS = "Sorry {} already exists!"
 RELATIONSHIPS = set(["father", "mother", "grandfather", "grandmother",
                      "daughters", "sons", "brothers", "sisters",
                      "uncles", "aunts", "cousins", "grandsons",
@@ -31,30 +31,31 @@ class Tree:
             if wife in self.data.keys():
                 raise ValueError(str.format(
                     "Both {0} and {1} already exist in your family tree.",
-                    husband, wife))
+                    husband.title(), wife.title()))
 
         if husband not in self.data.keys():
             if wife not in self.data.keys():
                 raise ValueError(str.format(
                     "Neither {0} and {1} exist in your family tree.",
-                    husband, wife))
+                    husband.title(), wife.title()))
 
         if husband in self.data:
+            self.ispresent(wife)
             self.data[husband]["wife"] = wife
             self.data[wife] = {"husband": husband}
             return str.format(WELCOME, wife.title())
 
         if wife in self.data:
+            self.ispresent(husband)
             self.data[wife]["husband"] = husband
             self.data[husband] = {"wife": wife}
             return str.format(WELCOME, husband.title())
 
     def add_child(self, child, parent , relation):
-
-        if self.ispresent(child):
-            return CHILD_EXISTS
-        
+   
         self.isvalid(parent, relation)
+        if child in self.data[parent][relation]:
+            raise ValueError(str.format(PERSON_EXISTS, child.title()))
 
         spouse_name = ""
         spouse_relation = ""
@@ -79,13 +80,13 @@ class Tree:
 
         if person not in self.data:
             raise ValueError(str.format("{} is a not a member of this family.",
-                             person))
+                             person.title()))
 
         if relation not in RELATIONSHIPS:
-            raise KeyError(
-                str.format("Sorry, {} not a supported relationship type",relation))
+            raise KeyError(str.format("Sorry, {} not a supported relationship type",
+                             relation.title()))
 
     def ispresent(self, person):
 
         if person in self.data:
-            return True
+            raise ValueError(str.format(PERSON_EXISTS, person))
